855e0c57d963a4fc47a6108c1f0bb4e89f3ba6a9
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-05 16:55:42 +0800

    Fix lint issue

--- onemark/api/api.py
+++ onemark/api/api.py
@@ -5,5 +5,7 @@ api_blueprint = Blueprint('api', __name__)
 
 
 @api_blueprint.route('/notebooks', methods=['GET'])
-def notebooks():
-    return jsonify(microsoft_graph.get('me/onenote/notebooks').data.get('value'))
+def get_notebooks():
+    notebooks = microsoft_graph.get('me/onenote/notebooks').data.get('value')
+
+    return jsonify(notebooks)
