d39adc69cf9d1cbc6c0e083e40649fadc7f0fc96
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-21 11:33:10 +0800

    Auto refresh token

--- markote/api/page.py
+++ markote/api/page.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone
 import io
 import json
 from flask import jsonify, request
@@ -15,8 +15,7 @@ MARKDOWN_FILE_OBJECT_HTML = '<object data-id="markdown-file" ' \
 @api_blueprint.route('/sections/<section_id>/pages', methods=['POST'])
 def create_page(section_id):
     page = request.json
-    utc = datetime.datetime.utcnow()
-    created_at = utc.replace(tzinfo=datetime.timezone.utc).isoformat()
+    created_at = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
     content = '''
         <!DOCTYPE html>
         <html>
--- markote/oauth.py
+++ markote/oauth.py
@@ -7,14 +7,20 @@ def fetch_token(name):
     return session.get('token')
 
 
+def update_token(token, name):
+    session['token'] = token
+
+
 client_id = os.environ.get('CLIENT_ID') or 'client id'
 client_secret = os.environ.get('CLIENT_SECRET') or 'client secret'
 access_token_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+refresh_token_url = \
+    'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 authorize_url = \
     'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
 scope = 'Notes.Create Notes.Read User.Read offline_access'
 
-oauth = OAuth(fetch_token=fetch_token)
+oauth = OAuth(fetch_token=fetch_token, update_token=update_token)
 oauth.register(
     name='microsoft_graph',
     client_id=client_id,
@@ -22,4 +28,5 @@ oauth.register(
     api_base_url='https://graph.microsoft.com/v1.0/',
     access_token_url=access_token_url,
     authorize_url=authorize_url,
+    refresh_token_url=refresh_token_url,
     client_kwargs={'scope': scope})
