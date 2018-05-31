80cc3b18d6a992b72d7a426ca7385392368d5a66
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-31 17:22:44 +0800

    Add settings dropdown

--- markote/static/js/src/components/Navigation.vue
+++ markote/static/js/src/components/Navigation.vue
@@ -1,7 +1,21 @@
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-end">
-            <a class="navbar-item">{{ me.displayName }}</a>
+            <div class="navbar-item dropdown is-hoverable">
+                <div class="dropdown-trigger">
+                    <button class="button" aria-haspopup="true" aria-controls="settings">
+                        <span>{{ me.displayName }}</span>
+                        <span class="icon is-small">
+                            <i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                    </button>
+                </div>
+                <div class="dropdown-menu" id="settings" role="menu">
+                    <div class="dropdown-content">
+                        <a class="dropdown-item">Settings</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
 </template>
