7c41bb58d33a402cfc8333de25daa3f12a63fb15
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-05 22:23:25 +0800

    Update ci

--- install-cairo.ps1
+++ install-cairo.ps1
@@ -1,7 +1,7 @@
 $cairoVersion = "1.15.10"
 $url = ([string]::Format("https://github.com/preshing/cairo-windows/releases/download/{0}/cairo-windows-{0}.zip", $cairoVersion))
 $output = "cairo.zip"
-$cairoPath = Join-Path $PSScriptROOT ([string]::Format("cairo-windows-{0}/lib/x64", $cairoVersion))
+$cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/x64", $cairoVersion))
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $url -OutFile $output
