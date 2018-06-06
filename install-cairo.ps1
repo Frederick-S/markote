9218de311146d8432b8a45113ed1fa2a7b5cce89
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-06 08:43:03 +0800

    Print debug message in ci

--- install-cairo.ps1
+++ install-cairo.ps1
@@ -1,10 +1,11 @@
 $cairoVersion = "1.15.10"
-$url = ([string]::Format("https://github.com/preshing/cairo-windows/releases/download/{0}/cairo-windows-{0}.zip", $cairoVersion))
-$output = "cairo.zip"
+$cairoDownLoadUrl = ([string]::Format("https://github.com/preshing/cairo-windows/releases/download/{0}/cairo-windows-{0}.zip", $cairoVersion))
+$cairoOutputFileName = "cairo.zip"
 $architecture = If ([Environment]::Is64BitProcess) { "x64" } Else { "x86" }
+Write-Host $architecture
 $cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/{1}", $cairoVersion, $architecture))
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $url -OutFile $output
-Expand-Archive $output -DestinationPath .
+Invoke-WebRequest -Uri $cairoDownLoadUrl -OutFile $cairoOutputFileName
+Expand-Archive $cairoOutputFileName -DestinationPath .
 $env:Path += ";" + $cairoPath
