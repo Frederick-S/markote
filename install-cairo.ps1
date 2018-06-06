9ca2409356640980607d41d64c9610b51090c8fe
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-06 08:53:13 +0800

    Update ci script

--- install-cairo.ps1
+++ install-cairo.ps1
@@ -1,9 +1,9 @@
 $cairoVersion = "1.15.10"
 $cairoDownLoadUrl = ([string]::Format("https://github.com/preshing/cairo-windows/releases/download/{0}/cairo-windows-{0}.zip", $cairoVersion))
 $cairoOutputFileName = "cairo.zip"
-$osArchitecture = If ($env:PYTHON_ARCH == 32) { "x86" } Else { "x64" }
-Write-Host $osArchitecture
-$cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/{1}", $cairoVersion, $osArchitecture))
+$pythonArchitecture = If ($env:PYTHON_ARCH == 32) { "x86" } Else { "x64" }
+Write-Host $pythonArchitecture
+$cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/{1}", $cairoVersion, $pythonArchitecture))
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $cairoDownLoadUrl -OutFile $cairoOutputFileName
