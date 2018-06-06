9234aac2debf5b4eff68d05a42466a0d3a2f66e4
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-06 08:39:26 +0800

    Check os architecture in ci

--- install-cairo.ps1
+++ install-cairo.ps1
@@ -1,7 +1,8 @@
 $cairoVersion = "1.15.10"
 $url = ([string]::Format("https://github.com/preshing/cairo-windows/releases/download/{0}/cairo-windows-{0}.zip", $cairoVersion))
 $output = "cairo.zip"
-$cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/x64", $cairoVersion))
+$architecture = If ([Environment]::Is64BitProcess) { "x64" } Else { "x86" }
+$cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/{1}", $cairoVersion, $architecture))
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $url -OutFile $output
