bf350c4f7dfd0988261fce949851ca836b6eb818
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-06 08:57:57 +0800

    Update install-cairo.ps1

--- install-cairo.ps1
+++ install-cairo.ps1
@@ -1,7 +1,7 @@
 $cairoVersion = "1.15.10"
 $cairoDownLoadUrl = ([string]::Format("https://github.com/preshing/cairo-windows/releases/download/{0}/cairo-windows-{0}.zip", $cairoVersion))
 $cairoOutputFileName = "cairo.zip"
-$pythonArchitecture = If ($env:PYTHON_ARCH == 32) { "x86" } Else { "x64" }
+$pythonArchitecture = If ($env:PYTHON_ARCH -eq 32) { "x86" } Else { "x64" }
 Write-Host $pythonArchitecture
 $cairoPath = Join-Path (Get-Item -Path ".\").FullName ([string]::Format("cairo-windows-{0}/lib/{1}", $cairoVersion, $pythonArchitecture))
 
