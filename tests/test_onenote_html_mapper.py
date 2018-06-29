9a2dfb2011c2ecdd1e9c97b773a285d865f589e5
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-29 10:46:24 +0800

    Test onenote html mapper

---
+++ tests/test_onenote_html_mapper.py
@@ -0,0 +1,24 @@
+import unittest
+from markote.onenote_html_mapper import OneNoteHtmlMapper
+from pyquery import PyQuery
+
+
+class OneNoteHtmlMapperTestCase(unittest.TestCase):
+    def test_convert_svg_to_image(self):
+        html = '''
+            <div>
+                <p>
+                    <svg width="100" height="100">
+                        <circle cx="50" cy="50" r="40" stroke="green"
+                         stroke-width="4" fill="yellow" />
+                    </svg>
+                </p>
+            </div>
+        '''
+        document = PyQuery(html)
+        onenote_html_mapper = OneNoteHtmlMapper(document)
+
+        onenote_html_mapper.convert()
+
+        self.assertTrue('img' in onenote_html_mapper.get_html())
+        self.assertFalse('svg' in onenote_html_mapper.get_html())
