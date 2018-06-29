2503571753b87539f51f4499f72ef08a875d4d21
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-29 15:31:46 +0800

    Test api with invalid client credential

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -11,3 +11,15 @@ class AuthTestCase(unittest.TestCase):
         response = self.client.get('/')
 
         self.assertEqual(302, response.status_code)
+
+    def test_api_with_invalid_client_credential(self):
+        with self.client.session_transaction() as session:
+            session['token'] = {
+                'token_type': 'Bearer',
+                'expires_in': '3600',
+                'access_token': 'abc'
+            }
+
+        response = self.client.get('/api/v1/notebooks')
+
+        self.assertEqual(401, response.status_code)
