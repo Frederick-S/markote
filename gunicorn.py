92e8e1cc58f5861a1bb07e7fcf98d4997db57226
Frederick-S <mao_xiaodan@hotmail.com>
2018-07-03 17:58:26 +0800

    Lint

--- gunicorn.py
+++ gunicorn.py
@@ -1,3 +1,4 @@
 bind = '0.0.0.0:5000'
 accesslog = 'app.log'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = \
+    '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
