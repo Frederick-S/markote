0bc8e8d3ea346d2eb583d7466dc778f86ea2db17
Frederick-S <mao_xiaodan@hotmail.com>
2018-07-21 10:04:20 +0800

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -2,3 +2,7 @@ bind = '0.0.0.0:5000'
 accesslog = 'app.log'
 access_log_format = \
     '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+raw_env = [
+    'GRAPH_CLIENT_ID=abc',
+    'GRAPH_CLIENT_SECRET=abc'
+]
