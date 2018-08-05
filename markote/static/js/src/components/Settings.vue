a55c1fa9646723cf5156e590039473c52744aed5
Frederick-S <mao_xiaodan@hotmail.com>
2018-08-05 19:18:19 +0800

    Upgrade dependencies

--- markote/static/js/src/components/Settings.vue
+++ markote/static/js/src/components/Settings.vue
@@ -10,6 +10,7 @@
                     <option>Clouds</option>
                 </b-select>
             </b-field>
+            <div id="editor-example" class="editor-example"></div>
             <b-field label="Code Theme">
                 <b-select expanded>
                     <option>Default</option>
@@ -26,13 +27,34 @@
 
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator'
+    import Config from '../models/config'
+    import configStore from '../stores/config'
 
     @Component
     export default class Settings extends Vue {
+        private editor!: AceAjax.Editor
+
+        get config(): Config {
+            return configStore.state.config
+        }
+
         private close() {
             const $parent: any = this.$parent
 
             $parent.close()
         }
+
+        private mounted() {
+            this.editor = ace.edit('editor-example')
+            this.editor.setTheme(`ace/theme/${this.config.editorTheme}`)
+            this.editor.session.setMode('ace/mode/markdown')
+        }
     }
 </script>
+
+<style>
+    .editor-example {
+        height: 200px;
+        border: 1px solid #cccccc;
+    }
+</style>
