94038e82b567dacfeca15893ec1d7dc55abad3e9
Frederick-S <mao_xiaodan@hotmail.com>
2019-06-01 15:53:14 +0800

    Pass env to gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,8 +1,11 @@
+import os
+
+
 bind = '0.0.0.0:5000'
 accesslog = 'app.log'
 access_log_format = \
     '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 raw_env = [
-    'GRAPH_CLIENT_ID=abc',
-    'GRAPH_CLIENT_SECRET=abc'
+    'GRAPH_CLIENT_ID=' + os.getenv('GRAPH_CLIENT_ID'),
+    'GRAPH_CLIENT_SECRET=' + os.getenv('GRAPH_CLIENT_SECRET')
 ]
