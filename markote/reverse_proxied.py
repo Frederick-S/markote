a20670d8a461b9c0400d72400b67407a68c198d8
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-29 02:26:52 +0000

    fix: read url scheme from env

--- markote/reverse_proxied.py
+++ markote/reverse_proxied.py
@@ -1,9 +1,13 @@
+import os
+
+
 class ReverseProxied(object):
     def __init__(self, app):
         self.app = app
 
     def __call__(self, environ, start_response):
-        scheme = environ.get('HTTP_X_SCHEME', '')
+        scheme = os.environ.get('MARKOTE_URL_SCHEME') \
+            or environ.get('HTTP_X_SCHEME', '')
 
         if scheme:
             environ['wsgi.url_scheme'] = scheme
